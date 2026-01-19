875b080506532988b4ff44baa77e94f56036c95c
KozyTweaks <philwilliams315@gmail.com>
2026-01-18 21:34:18 -0500

    Update kozy_tweaks_landing_page.jsx

--- kozy_tweaks_landing_page.jsx
+++ kozy_tweaks_landing_page.jsx
@@ -134,16 +134,3 @@ export default function Kozytweaks() {
     </div>
   );
 }
-import Kozytweaks from "../Kozytweaks";
-
-export default function Page() {
-  return <Kozytweaks />;
-}
-{
-  "dependencies": {
-    "next": "^13.5.0",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    ...
-  }
-}
